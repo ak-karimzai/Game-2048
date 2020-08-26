bba5c2f27d16834f7647c747374034783c9c36b8
Azamat Kostoev <azamat.kostoev.ing@gmail.com>
2020-08-26 22:41:20 +0300

    add UI

--- UI.py
+++ UI.py
@@ -1,216 +1,112 @@
-
 import pygame, random, sys
 from pygame.locals import *
 
-WINDOWWIDTH = 600
-WINDOWHEIGHT = 600
-TEXTCOLOR = (255, 255, 255)
-BACKGROUNDCOLOR = (0, 0, 0)
+WINDOWHEIGHT = 360
+TEXTCOLOR = (0, 0, 0)
+BACKGROUNDCOLOR = (192, 192, 192)
 FPS = 40
-BADDIEMINSIZE = 30
-BADDIEMAXSIZE = 60
-BADDIEMINSPEED = 1
-BADDIEMAXSPEED = 8
-ADDNEWBADDIERATE = 15
-PLAYERMOVERATE = 5
+
+h = 10
+
+RECT_SIZE = (54, 54)
+
+WINDOWWIDTH = 5*h + 4*RECT_SIZE[0]
+
+# Матрица игрового поля
+matr = [[0,0,2,4],
+        [2,0,256,32],
+        [0,2,64,128],
+        [2,8,8,16]]
+
+
+def matf(x):
+    if x > 255:
+        return 0
+    if x < 0:
+        return 0
+    return x
 
 def terminate():
     pygame.quit()
     sys.exit()
 
+
 def waitForPlayerToPressKey():
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
-                terminate()
+                terminate    
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE: # Выход из программы
                     terminate()
                 return
 
-def playerHasHitBaddie(playerRect, baddies):
-    for b in baddies:
-        if playerRect.colliderect(b['rect']):
-            return True
-    return False
-
-def drawText(text, font, surface, x, y):
+# функция, которая рисует текст
+def drawText(text, font, surface, x, y, type):
     textobj = font.render(text, 1, TEXTCOLOR)
     textrect = textobj.get_rect()
-    textrect.topleft = (x, y)
+    if type == 'c':
+        textrect.topleft = (x - textrect.width/2, y - textrect.height/2)
+    else:
+        textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
 # запустить pygame, создать основное окно
 pygame.init()
 mainClock = pygame.time.Clock()
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-pygame.display.set_caption('Space Game')
 
-# настроить шрифты
-font = pygame.font.SysFont(None, 35)
+pygame.display.set_caption('2048')
 
-# загрузить изображения
-playerImage = pygame.image.load('player.png')
-playerRect = playerImage.get_rect()
-baddieImage = pygame.image.load('baddie.png')
+font = pygame.font.SysFont(None, 30)
 
-exp_anim = []
+# Переменные, в которых хранятся счет и рекорд
+score = 0
+topScore = 0
 
-exp_sprite = pygame.image.load("explosion.png").convert_alpha()
+while True: # the game loop runs while the game part is playing
+    windowSurface.fill(BACKGROUNDCOLOR)
+    rect_y = 100
 
-image_s = 100
+    drawText('Счет: %s' % (score), font, windowSurface, 15, 10, '')
+    drawText('Рекорд: %s' % (topScore), font, windowSurface, 15, 40, '')
 
-x = 0
-y = 0
+    pygame.draw.rect(windowSurface, (60, 60, 60), (h/2, rect_y - h/2, RECT_SIZE[0]*4 + h*4, RECT_SIZE[1]*4 + h*4))
 
-for i in range(5):
-    x = 0
-    for j in range(5):
-        im_rect = (x, y, image_s, image_s)
-        exp_anim.append(exp_sprite.subsurface(im_rect))
-        x += image_s
-    y += image_s
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            terminate()
 
-# показать 
-drawText('игра space game, автор: Костоев А.И', font, windowSurface, (WINDOWWIDTH / 3 - 130), (WINDOWHEIGHT / 3))
-drawText('Нажмите клавишу "a" для начала игры.', font, windowSurface, (WINDOWWIDTH / 3 - 110) - 30, (WINDOWHEIGHT / 3) + 50)
+    # Цикл, рисующий клетки игрового поля
+    for i in range(4):
+        rect_x = h
+        for j in range(4):
+            rect_color = (255, 255, 255)
+            if matr[i][j] != 0:
+                rect_color = (255, 255, matf(255 - matr[i][j]*10))
+            if (rect_color[2] == 0):
+                rect_color = (255, matf(255 - matr[i][j]), 0)
+            if (rect_color[1] == 0):
+                rect_color = (matf(255 - matr[i][j]/10), 0, 0)
+
+            if (matr[i][j] == 0):
+                rect_color = (128, 128, 128)
+                pygame.draw.rect(windowSurface, rect_color, (rect_x, rect_y, RECT_SIZE[0], RECT_SIZE[1]))
+            else:
+                pygame.draw.rect(windowSurface, rect_color, (rect_x, rect_y, RECT_SIZE[0], RECT_SIZE[1]))
+                drawText(str(matr[i][j]), pygame.font.SysFont(None, 30), windowSurface, rect_x + 27, rect_y + 27, 'c')
+
+            rect_x += RECT_SIZE[0] + h
+        rect_y += RECT_SIZE[0] + h
+
+    pygame.display.update()
+
+    mainClock.tick(FPS)
+
+'''
+drawText('ИГРА ОКОНЧЕНА', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+drawText('Нажмите клавишу "a" чтобы начать заново.', font, windowSurface, (WINDOWWIDTH / 3 - 140), (WINDOWHEIGHT / 3) + 50)
 pygame.display.update()
 waitForPlayerToPressKey()
+'''
 
-bg = pygame.image.load("back.png")
-
-topScore = 0
-while True:
-    # set up the start of the game
-    baddies = []
-    score = 0
-    playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 70)
-    moveLeft = moveRight = moveUp = moveDown = False
-    reverseCheat = slowCheat = False
-    baddieAddCounter = 0
-
-    while True: # the game loop runs while the game part is playing
-        score += 1 # increase score
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                terminate()
-
-            if event.type == KEYDOWN:
-                if event.key == ord('z'):
-                    reverseCheat = True
-                if event.key == ord('x'):
-                    slowCheat = True
-                if event.key == K_LEFT or event.key == ord('a'):
-                    moveRight = False
-                    moveLeft = True
-                if event.key == K_RIGHT or event.key == ord('d'):
-                    moveLeft = False
-                    moveRight = True
-                if event.key == K_UP or event.key == ord('w'):
-                    moveDown = False
-                    moveUp = True
-                if event.key == K_DOWN or event.key == ord('s'):
-                    moveUp = False
-                    moveDown = True
-
-            if event.type == KEYUP:
-                if event.key == ord('z'):
-                    reverseCheat = False
-                    score = 0
-                if event.key == ord('x'):
-                    slowCheat = False
-                    score = 0
-                if event.key == K_ESCAPE:
-                        terminate()
-
-                if event.key == K_LEFT or event.key == ord('a'):
-                    moveLeft = False
-                if event.key == K_RIGHT or event.key == ord('d'):
-                    moveRight = False
-                if event.key == K_UP or event.key == ord('w'):
-                    moveUp = False
-                if event.key == K_DOWN or event.key == ord('s'):
-                    moveDown = False
-
-        # Add new baddies at the top of the screen, if needed.
-        if not reverseCheat and not slowCheat:
-            baddieAddCounter += 1
-        if baddieAddCounter == ADDNEWBADDIERATE:
-            baddieAddCounter = 0
-            baddieSize = random.randint(BADDIEMINSIZE, BADDIEMAXSIZE)
-            newBaddie = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-baddieSize), 0 - baddieSize, baddieSize, baddieSize),
-                        'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
-                        'surface':pygame.transform.scale(baddieImage, (baddieSize, baddieSize)),
-                        }
-
-            baddies.append(newBaddie)
-
-        # Move the player around.
-        if moveLeft and playerRect.left > 0:
-            playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
-        if moveRight and playerRect.right < WINDOWWIDTH:
-            playerRect.move_ip(PLAYERMOVERATE, 0)
-        if moveUp and playerRect.top > 0:
-            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
-        if moveDown and playerRect.bottom < WINDOWHEIGHT:
-            playerRect.move_ip(0, PLAYERMOVERATE)
-
-        # Move the baddies down.
-        for b in baddies:
-            if not reverseCheat and not slowCheat:
-                b['rect'].move_ip(0, b['speed'])
-            elif reverseCheat:
-                b['rect'].move_ip(0, -5)
-            elif slowCheat:
-                b['rect'].move_ip(0, 1)
-
-         # Delete baddies that have fallen past the bottom.
-        for b in baddies[:]:
-            if b['rect'].top > WINDOWHEIGHT:
-                baddies.remove(b)
-
-        # Draw the game world on the window.
-        windowSurface.fill(BACKGROUNDCOLOR)
-        windowSurface.blit(bg, (0, 0))
-
-        # Draw the score and top score.
-        drawText('Счет: %s' % (score), font, windowSurface, 10, 0)
-        drawText('Рекорд: %s' % (topScore), font, windowSurface, 10, 40)
-
-        # Draw the player's rectangle
-        windowSurface.blit(playerImage, playerRect)
-
-        # Draw each baddie
-        for b in baddies:
-            windowSurface.blit(b['surface'], b['rect'])
-
-        pygame.display.update()
-
-        # Check if any of the baddies have hit the player.
-        if playerHasHitBaddie(playerRect, baddies):
-            if score > topScore:
-                topScore = score # set new top score
-            break
-
-        mainClock.tick(FPS)
-
-    for i in range(len(exp_anim)):
-        windowSurface.fill(BACKGROUNDCOLOR)
-        windowSurface.blit(bg, (0, 0))
-
-        drawText('Счет: %s' % (score), font, windowSurface, 10, 0)
-        drawText('Рекорд: %s' % (topScore), font, windowSurface, 10, 40)
-
-        windowSurface.blit(exp_anim[i], (playerRect[0] - image_s / 2  + 25, playerRect[1] - image_s / 2 + 32))
-
-        for b in baddies:
-            windowSurface.blit(b['surface'], b['rect'])
-
-        mainClock.tick(20)
-        pygame.display.update()
-
-    drawText('ИГРА ОКОНЧЕНА', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
-    drawText('Нажмите клавишу "a" чтобы начать заново.', font, windowSurface, (WINDOWWIDTH / 3 - 140), (WINDOWHEIGHT / 3) + 50)
-    pygame.display.update()
-    waitForPlayerToPressKey()
